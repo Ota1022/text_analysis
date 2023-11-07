0cc048f249c2fe796b8f0a2e623fce878fffdec1
Ota1022 <55938456+Ota1022@users.noreply.github.com>
2023-11-07 21:18:09 +0900

    update dataset

--- data/text/remove_break.py
+++ data/text/remove_break.py
@@ -1,5 +1,3 @@
-# Open the original file in read mode and the new file in write mode
-with open('data/2011.txt', 'r') as original_file, open('data/2011_2.txt', 'w') as new_file:
+with open('pre.txt', 'r') as original_file, open('done.txt', 'w') as new_file:
     for line in original_file:
-        # Remove the line break and spaces from each line and write it to the new file
         new_file.write(line.replace(' ', '').strip())
